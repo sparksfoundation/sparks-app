7c4e558c53eb51f60f459c95a6a7d876ef0950bd
michaeldumalag <michael.dumalag@gmail.com>
2023-06-22 15:15:11 -0700

    Add Identity settings page skeleton

---
+++ src/views/user/settings/Details.tsx
@@ -0,0 +1,8 @@
+import { Card, H4 } from "sparks-ui";
+import { useUser } from "@stores/user";
+
+export const Details = () => {
+  return (
+    <H4 className="text-fg-800 dark:text-fg-300 font-bold mb-4">Details</H4>
+  );
+};
---
+++ src/views/user/settings/ExportIdentity.tsx
@@ -0,0 +1,9 @@
+import { H4 } from "sparks-ui";
+
+export const ExportIdentity = () => {
+  return (
+    <H4 className="text-fg-800 dark:text-fg-300 font-bold mb-4">
+      Export Identity
+    </H4>
+  );
+};
