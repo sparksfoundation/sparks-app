bb7f8de3f7cda62e3d76d8b4fb27edff0afa6c35
sparksdev <dev@sparks.dev>
2023-07-01 01:30:27 -0400

    limit 1 member, parse name from store

--- src/stores/members.ts
+++ src/stores/members.ts
@@ -1,6 +1,7 @@
 import { storage } from './indexedDb';
 import { createJSONStorage, persist } from 'zustand/middleware';
 import { create } from 'zustand';
+import { Buffer } from 'buffer';
 
 export type Member = {
   salt: string;
@@ -10,7 +11,7 @@ export type Member = {
 
 export interface MemberStore {
   members: Member[];
-  getMembers: (state: MemberStore) => Member[];
+  getMembers: () => Member[];
   addMember: (member: Member) => void;
   removeMember: (salt: string) => void;
 }
@@ -18,8 +19,17 @@ export interface MemberStore {
 export const useMembers = create<MemberStore>()(
   persist((set, get) => ({
     members: [],
-    getMembers: () => get().members,
+    getMembers: () => {
+      const members = get().members
+      return members.map(({ data, name, salt }) => ({
+        data,
+        name: Buffer.from(name, 'base64').toString('utf-8'),
+        salt,
+      }))
+    },
     addMember: (member: Member) => {  
+      // allow only one identity for now
+      if (get().members.length > 0) return;
       set({ members: [...get().members, member] })
     },
     removeMember: (salt: string) => {
