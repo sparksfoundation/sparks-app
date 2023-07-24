935e74c773bc41d8cba4af9eceb268f262429216
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2023-07-24 10:23:23 -0400

    fix build issues

--- src/components/Toast/WebRTCOpen.tsx
+++ src/components/Toast/WebRTCOpen.tsx
@@ -1,10 +1,9 @@
 import { Paths } from "@routes/paths";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { ChannelRequestEvent } from "sparks-sdk/channels/ChannelEvent";
 import { Button, P } from "sparks-ui";
 
-export const WebRTCOpen = ({ event, resolve, reject }: { event: ChannelRequestEvent, resolve: Function, reject: () => void }) => {
+export const WebRTCOpen = ({ event, resolve, reject }: { event: any, resolve: Function, reject: () => void }) => {
   if (!event?.data?.identifier) return null;
   const peerId = `${event.data.identifier.slice(0, 6)}...${event.data.identifier.slice(-6)}`;
   const navigate = useNavigate();
@@ -29,10 +28,10 @@ export const webRTCOpenToaster = ({
   event,
   resolve,
   reject,
-}: { event: ChannelRequestEvent, resolve: ()=>void, reject?: ()=>void }) => {
+}: { event: any, resolve: ()=>void, reject?: ()=>void }) => {
   toast(({ closeToast }) =>
     <WebRTCOpen
-      event={event as ChannelRequestEvent}
+      event={event as any}
       resolve={resolve}
       reject={() => {
         if (reject) reject();
